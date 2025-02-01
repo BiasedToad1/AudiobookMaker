3ffd82714efdcb7fa58bb2d91e7fb63e15459fe9
BiasedToad1 <dimrain46@gmail.com>
2025-02-01 02:27:00 -0700

    cleaned up the code a bit

--- main.py
+++ main.py
@@ -3,36 +3,36 @@ import sys
 import time
 
 lim = 4000  # char limit
-fcount = 1
-multfiles = False
+fileCount = 1
+multipleFiles = False
 
 def removePageNums():
     tempLine = ""
-    with open("INPUT_TEXT.txt", "r") as file:
+    with open("INPUT_TEXT.txt", "r") as file: # opens the file and copies all text onto tempLine
         tempLine = file.read()
     with open("INPUT_TEXT.txt", "w") as file:
-        pgs = 0
+        pgs = 0 # keeps pg count
         try:
-            for i in range(0, len(tempLine) - 2):
+            for i in range(0, len(tempLine) - 2): # scans through tempLine to look for pg nums
                 if (tempLine[i] in '123456789') and (i == 0 or tempLine[i - 1] == '\n'):
-                        if tempLine[i + 1] == '\n': # 1 digit nums
-                            tempLine = tempLine[:i] + ' ' + tempLine[i + 1:]
+                    if tempLine[i + 1] == '\n': # 1 digit nums
+                        tempLine = tempLine[:i] + ' ' + tempLine[i + 1:]
+                        pgs += 1
+                    elif (tempLine[i + 1] in '1234567890,'):
+                        if tempLine[i + 2] == '\n': # 2 digit nums
+                            tempLine = tempLine[:i] + '  ' + tempLine[i + 2:]
                             pgs += 1
-                        elif (tempLine[i + 1] in '1234567890,'):
-                            if tempLine[i + 2] == '\n': # 2 digit nums
-                                tempLine = tempLine[:i] + '  ' + tempLine[i + 2:]
+                        elif (tempLine[i + 2] in '1234567890'):
+                            if tempLine[i + 3] == '\n': # 3 digit nums
+                                tempLine = tempLine[:i] + '  ' + tempLine[i + 3:]
                                 pgs += 1
-                            elif (tempLine[i + 2] in '1234567890'):
-                                if tempLine[i + 3] == '\n': # 3 digit nums
-                                    tempLine = tempLine[:i] + '  ' + tempLine[i + 3:]
+                            elif (tempLine[i + 3] in '123456789'):
+                                if tempLine[i + 4] == '\n': # 4 digit nums
+                                    tempLine = tempLine[:i] + '  ' + tempLine[i + 4:]
                                     pgs += 1
-                                elif (tempLine[i + 3] in '123456789'):
-                                    if tempLine[i + 4] == '\n': # 4 digit nums
-                                        tempLine = tempLine[:i] + '  ' + tempLine[i + 4:]
-                                        pgs += 1
-                                    elif (tempLine[i + 4] in '123456789' and tempLine[i + 5] == '\n'): # 4 digit nums w/ comma
-                                        tempLine = tempLine[:i] + '  ' + tempLine[i + 5:]
-                                        pgs += 1
+                                elif (tempLine[i + 4] in '123456789' and tempLine[i + 5] == '\n'): # 4 digit nums w/ comma
+                                    tempLine = tempLine[:i] + '  ' + tempLine[i + 5:]
+                                    pgs += 1
         except IndexError:
             pass
         finally:
@@ -55,13 +55,13 @@ def textDivider():
             count += 1
     print("files deleted")
 
+    # creates files
     with open("INPUT_TEXT.txt", "r") as inputText:
         count = 0
         file = 1
         extraLine = ""
 
         while True:
-            # creates files
             o = str(file) + ".txt"
             with open(o, "w") as outputText:
                 outputText.write(extraLine)
@@ -81,21 +81,42 @@ def textDivider():
                 break
 
         print(f"Created {file} files")
-        global fcount
-        global multfiles
-        fcount = int(file)
+        global fileCount
+        global multipleFiles
+        fileCount = int(file)
         if file > 1:
-            multfiles = True
+            multipleFiles = True
+
+def renamerZeros(files, num, zeros): # used to determine if and how many zeros should go before number
+    if zeros == False:
+        return num
+    if files < 100:
+        if num < 10:
+            tempstr = "0" + str(num)
+            return tempstr
+        else:
+            return num
+    if files < 1000 and num < 100 and num >= 10:
+        tempstr = "0" + str(num)
+        return tempstr
+    elif files < 1000 and num < 10:
+        tempstr = "00" + str(num)
+        return tempstr
+    else:
+        return num
 
 model = ""
 
 print("\n --- AUDIOBOOK MAKER --- \n      by BiasedToad\n    written in Python\n\nCopy Text into 'INPUT_TEXT.txt'")
+
 confirmation = input("Enter [Y] when ready: ")
 while not (confirmation == 'y' or confirmation == 'Y'):
         confirmation = input("Enter [Y] when ready: ")
+
 confirmation = input("Delete Page Numbers? [Experimental] (y/N): ")
 if (confirmation == 'y' or confirmation == 'Y'):
     removePageNums()
+
 confirmation = input("Split Audio Files? (y/N): ")
 if (confirmation == 'y' or confirmation == 'Y'):
     confirmation = input("Max Text Character Size? (4000 default): ")
@@ -107,21 +128,22 @@ else:
         tempLine = file.read()
     with open("1.txt", "w") as file:
         file.write(tempLine)
+
 while True:
     confirmation = input("Select a Voice Model [You can test out each voice in the 'Models/' directory]\n(a/d/hf/hm/k/custom): ")
-    if confirmation == 'a':
+    if confirmation == 'a' or confirmation == 'amy':
         model = "en_US-amy-medium"
         break
-    elif confirmation == 'd':
+    elif confirmation == 'd' or confirmation == 'danny':
         model = "en_US-danny-low"
         break
-    elif confirmation == 'hf':
+    elif confirmation == 'hf' or confirmation == 'hfc_female':
         model = "en_US-hfc_female-medium"
         break
-    elif confirmation == 'hm':
+    elif confirmation == 'hm' or confirmation == 'hfc_male':
         model = "en_US-hfc_male-medium"
         break
-    elif confirmation == 'k':
+    elif confirmation == 'k' or confirmation == kristin:
         model = "en_US-kristin-medium"
         break
     elif confirmation == 'custom':
@@ -142,15 +164,15 @@ while True:
     else:
         print("Not a Valid Model")
 
-ncv = ""
-if(multfiles):
+nameConvs = ""
+if(multipleFiles):
     confirmation = input("Rename audio files? (y/N): ")
     if (confirmation == 'y' or confirmation == 'Y'):
         while True:
-            ncv = input("Input Naming Conventions [# will be replaced with ascending numbers]\n(ex: v03c02_#): ")
+            nameConvs = input("Input Naming Conventions [# will be replaced with ascending numbers]\n(ex: v03c02_#): ")
             badfilename = False
             pdsn = False
-            for x in ncv:
+            for x in nameConvs:
                 if x == '#':
                     if pdsn == True:
                         print("Can't use more than one '#'")
@@ -164,7 +186,7 @@ if(multfiles):
                 break
 
 zeros = False
-if (not ncv == "") and (fcount >= 10):
+if (nameConvs != "") and (fileCount >= 10):
     confirmation = input("Put 0s before lower digit numbers? [1, 2... --> 01, 02...] (y/N): ")
     if (confirmation == 'y' or confirmation == 'Y'):
         zeros = True
@@ -172,34 +194,16 @@ if (not ncv == "") and (fcount >= 10):
 start = time.perf_counter()
 
 if sys.platform == "win32":
-    for i in range(1, int(fcount + 1)):
+    for i in range(1, int(fileCount + 1)):
         os.system("type " + str(i) + ".txt | .\\piperw\\piper.exe -m Models/" + model + ".onnx -f " + str(i) + ".wav")
         os.system("erase " + str(i) + ".txt")
-        if ncv == "":
-            os.system("move " + str(i) + ".wav completed/")
+        if nameConvs != "":
+            os.system("move " + str(i) + ".wav completed/" + nameConvs.replace("#", str(renamerZeros(fileCount, i, zeros))) + ".wav")
         else:
-            if zeros == True:
-                if fcount < 100:
-                    if i < 10:
-                        tempstr = "0" + str(i)
-                        os.system("move " + str(i) + ".wav completed/" + ncv.replace("#", tempstr) + ".wav")
-                    else:
-                        os.system("move " + str(i) + ".wav completed/" + ncv.replace("#", str(i)) + ".wav")
-                if fcount < 1000:
-                    if i < 100 and i >= 10:
-                        tempstr = "0" + str(i)
-                        os.system("move " + str(i) + ".wav completed/" + ncv.replace("#", tempstr) + ".wav")
-                    elif i < 10:
-                        tempstr = "00" + str(i)
-                        os.system("move " + str(i) + ".wav completed/" + ncv.replace("#", tempstr) + ".wav")
-                    else:
-                        os.system("move " + str(i) + ".wav completed/" + ncv.replace("#", str(i)) + ".wav")
-
-            if zeros == False:
-                os.system("move " + str(i) + ".wav completed/" + ncv.replace("#", str(i)) + ".wav")
+            os.system("move " + str(i) + ".wav completed/")
 
 elif sys.platform == "linux":
-    for i in range(1, int(fcount + 1)):
+    for i in range(1, int(fileCount + 1)):
         os.system("cat " + str(i) + ".txt | ./piper/piper -m Models/" + model + ".onnx -f " + str(i) + ".wav")
         os.system("ffmpeg -i " + str(i) + ".wav " + str(i) + ".mp3")
         o = str(i) + ".txt"
@@ -212,28 +216,10 @@ elif sys.platform == "linux":
             os.remove(o)
         except FileNotFoundError:
             pass
-        if ncv == "":
-            os.system("mv " + str(i) + ".mp3 completed/")
+        if nameConvs != "":
+            os.system("mv " + str(i) + ".mp3 completed/" + nameConvs.replace("#", str(renamerZeros(fileCount, i, zeros))) + ".mp3")
         else:
-            if zeros == True:
-                if fcount < 100:
-                    if i < 10:
-                        tempstr = "0" + str(i)
-                        os.system("mv " + str(i) + ".mp3 completed/" + ncv.replace("#", tempstr) + ".mp3")
-                    else:
-                        os.system("mv " + str(i) + ".mp3 completed/" + ncv.replace("#", str(i)) + ".mp3")
-                if fcount < 1000:
-                    if i < 100 and i >= 10:
-                        tempstr = "0" + str(i)
-                        os.system("mv " + str(i) + ".mp3 completed/" + ncv.replace("#", tempstr) + ".mp3")
-                    elif i < 10:
-                        tempstr = "00" + str(i)
-                        os.system("mv " + str(i) + ".mp3 completed/" + ncv.replace("#", tempstr) + ".mp3")
-                    else:
-                        os.system("mv " + str(i) + ".mp3 completed/" + ncv.replace("#", str(i)) + ".mp3")
-
-            if zeros == False:
-                os.system("mv " + str(i) + ".mp3 completed/" + ncv.replace("#", str(i)) + ".mp3")
+            os.system("mv " + str(i) + ".mp3 completed/")
 
 end = time.perf_counter()
 time = int(end - start)
